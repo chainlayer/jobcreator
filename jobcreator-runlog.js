c131802687cf387bdc521ff64d946a52c762c017
Peter van Mourik <tyrion70@gmail.com>
2020-08-28 11:03:19 +0200

    add FM

--- jobcreator-runlog.js
+++ jobcreator-runlog.js
@@ -28,48 +28,96 @@ for (var a = 0; a < opkeys.length; a++) {
 // Find job
 for (var a = 0; a < contractkeys.length; a++) {
   if (contracts[contractkeys[a]].marketing.path == job) {
-    // found the job, create jobspec
     let jobspec = new Object();
-    jobspec.initiators = new Array();
-    jobspec.initiators.push({
-      "type": "runlog",
-      "params": {
-        "address": oracleAddress
-      }
-    })
-    jobspec.tasks = new Array();
-    jobspec.tasks.push({
-      "type": bridge,
-      "params": {
-        "from": contracts[contractkeys[a]].marketing.pair[0],
-        "to": contracts[contractkeys[a]].marketing.pair[1]
-      }
-    })
-    jobspec.tasks.push({
-      "type": "copy",
-      "params": {
-        "copyPath": [
-          "result"
-        ]
-      }
-    })
-    jobspec.tasks.push({
-      "type": "multiply",
-      "params": {
-        "times": Math.pow(10, contracts[contractkeys[a]].decimals)
-      }
-    })
-    jobspec.tasks.push({
-      "type": "ethint256",
-      "params": {
-      }
-    })
-    jobspec.tasks.push({
-      "type": "ethtx",
-      "params": {
-      }
-    })
+    if (contracts[contractkeys[a]].contractVersion==2) {
+      // found the job, create jobspec
+      jobspec.initiators = new Array();
+      jobspec.initiators.push({
+        "type": "runlog",
+        "params": {
+          "address": oracleAddress
+        }
+      })
+      jobspec.tasks = new Array();
+      jobspec.tasks.push({
+        "type": bridge,
+        "params": {
+          "from": contracts[contractkeys[a]].marketing.pair[0],
+          "to": contracts[contractkeys[a]].marketing.pair[1]
+        }
+      })
+      jobspec.tasks.push({
+        "type": "copy",
+        "params": {
+          "copyPath": [
+            "result"
+          ]
+        }
+      })
+      jobspec.tasks.push({
+        "type": "multiply",
+        "params": {
+          "times": Math.pow(10, contracts[contractkeys[a]].decimals)
+        }
+      })
+      jobspec.tasks.push({
+        "type": "ethint256",
+        "params": {
+        }
+      })
+      jobspec.tasks.push({
+        "type": "ethtx",
+        "params": {
+        }
+      })
+    }
+    if (contracts[contractkeys[a]].contractVersion==3) {
+      // found the job, create jobspec
+      jobspec.initiators = new Array();
+      jobspec.initiators.push({
+        "type": "fluxmonitor",
+        "params": {
+          "address": contractkeys[a],
+          "requestData": {
+            "data": {
+              "from": contracts[contractkeys[a]].marketing.pair[0],
+              "to": contracts[contractkeys[a]].marketing.pair[1]
+            },
+          },
+          "feeds": [
+            {
+              "bridge": bridge
+            }
+          ],
+          "threshold": contracts[contractkeys[a]].deviationThreshold,
+          "precision": contracts[contractkeys[a]].decimals,
+          "idleTimer": (contracts[contractkeys[a]].heartbeat==null?
+             {"disabled": true,"duration": "0s"}:
+             { "duration": contracts[contractkeys[a]].heartbeat }
+          ),
+          "pollTimer": {
+            "period": "1m"
+          }
+        }
+      })
+      jobspec.tasks = new Array();
+      jobspec.tasks.push({
+        "type": "multiply",
+        "params": {
+          "times": Math.pow(10, contracts[contractkeys[a]].decimals)
+        }
+      })
+      jobspec.tasks.push({
+        "type": "ethint256",
+        "params": {
+        }
+      })
+      jobspec.tasks.push({
+        "type": "ethtx",
+        "params": {
+        }
+      })
+    }
     console.log(JSON.stringify(jobspec))
-    
-  }
+ }
 }
