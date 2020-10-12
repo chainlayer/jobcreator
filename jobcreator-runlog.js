b11abdbed87bb22d18005aef0c64e45bb960f6ac
Peter van Mourik <tyrion70@gmail.com>
2020-10-12 08:51:27 +0200

    Fix polltimer on null deviations

--- jobcreator-runlog.js
+++ jobcreator-runlog.js
@@ -59,10 +59,6 @@ for (var a = 0; a < contractkeys.length; a++) {
       jobspec.tasks = new Array();
       jobspec.tasks.push({
         "type": bridgename,
-        "params": {
-          "from": contracts[contractkeys[a]].marketing.pair[0],
-          "to": contracts[contractkeys[a]].marketing.pair[1]
-        }
       })
       jobspec.tasks.push({
         "type": "copy",
@@ -106,22 +102,19 @@ for (var a = 0; a < contractkeys.length; a++) {
         "type": "fluxmonitor",
         "params": {
           "address": contractkeys[a],
-          "requestData": {
-            "data": {
-              "from": contracts[contractkeys[a]].marketing.pair[0],
-              "to": contracts[contractkeys[a]].marketing.pair[1]
-            },
-          },
           "feeds": feeds,
-          "threshold": contracts[contractkeys[a]].deviationThreshold,
+          "threshold": (contracts[contractkeys[a]].deviationThreshold==null || contracts[contractkeys[a]].deviationThreshold==0?
+             0.1:
+             contracts[contractkeys[a]].deviationThreshold),
           "precision": contracts[contractkeys[a]].decimals,
           "idleTimer": (contracts[contractkeys[a]].heartbeat==null?
              { "duration": "24h" }:
              { "duration": contracts[contractkeys[a]].heartbeat }
           ),
-          "pollTimer": {
-            "period": "1m"
-          }
+          "pollTimer": (contracts[contractkeys[a]].deviationThreshold==null || contracts[contractkeys[a]].deviationThreshold==0?
+             { "disabled" : true }:
+             { "period": "1m" }
+          )
         }
       })
       jobspec.tasks = new Array();
